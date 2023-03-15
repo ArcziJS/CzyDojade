f297eddd11332ca774b6dc1bbd1e4b01260b4684
Artur Siudak <artur.siudak@student.po.edu.pl>
2023-03-16 00:44:00 +0100

    Adding a very simple car selection page.

---
+++ CzyDojade/CarSelector.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+
+namespace CzyDojade
+{
+    [Activity(Label = "CarSelector", Theme = "@style/AppTheme")]
+    public class CarSelector : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.car_selection);
+
+            Button getBackButton = FindViewById<Button>(Resource.Id.getBackButton);
+            getBackButton.Click += delegate
+            {
+                StartActivity(typeof(MainActivity));
+            };
+        }
+    }
+}
--- CzyDojade/MainActivity.cs
+++ CzyDojade/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Views;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 
 namespace CzyDojade
@@ -11,10 +13,17 @@ namespace CzyDojade
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.main_screen);
+            
+            ImageButton selectCarButton = FindViewById<ImageButton>(Resource.Id.SelectCarButton);
+            selectCarButton.Click += delegate
+            {
+                StartActivity(typeof(CarSelector));
+            };
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
