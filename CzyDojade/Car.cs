4f704c59a56dee4d9e7f81a775092c8433ecfc58
Artur Siudak <artur.siudak@student.po.edu.pl>
2023-03-24 18:55:16 +0100

    Displaying all (11) cars from the database.

---
+++ CzyDojade/Car.cs
@@ -0,0 +1,57 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Security;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CzyDojade
+{
+    internal class Car
+    {
+        int id;
+        string producer;
+        string model;
+        string range;
+        string url;
+        /// <summary>
+        /// Create a new car object with atributes fetched from database.
+        /// </summary>
+        /// <param name="connection">MySQL connection object for fetching car details purposes.</param>
+        /// <param name="carId">Car ID needed in MySQL query string to get certain car.</param>
+        public Car(MySqlConnection connection, int carId)
+        {
+
+
+
+            var query = connection.CreateCommand();
+            string queryString = "SELECT * FROM samochody where id=" + carId;
+            query.CommandText = queryString;
+
+            var result = query.ExecuteReader();
+
+            result.Read();
+
+            this.id = carId;
+            this.producer = result.GetString("marka").ToString();
+            this.model = result.GetString("model").ToString();
+            this.range = result.GetString("zasieg").ToString();
+            //this.url = result.GetString("obrazek").ToString();
+
+            result.Close();
+        }
+
+        public int GetId() { return id; }
+        public string GetProducer() { return producer; }
+        public string GetModel() { return model; }
+        public string GetRange() { return range; }
+        public string GetUrl() { return url; }
+
+    }
+}
--- CzyDojade/CarSelector.cs
+++ CzyDojade/CarSelector.cs
@@ -14,45 +14,178 @@ namespace CzyDojade
 
             SetContentView(Resource.Layout.car_selection);
 
-            TextView Car = FindViewById<TextView>(Resource.Id.textView1);
-            TextView Range = FindViewById<TextView>(Resource.Id.textView2);
-            Button getBackButton = FindViewById<Button>(Resource.Id.getBackButton);
+            #region TextViews car producer + model
 
-            MySqlConnection connection = new MySqlConnection("Server=db4free.net;Port=3306;Database=samochody;Uid=czydojade;Pwd=czydojade;");
+            TextView TextViewCar1 = FindViewById<TextView>(Resource.Id.textViewCar1);
+            TextView TextViewCar2 = FindViewById<TextView>(Resource.Id.textViewCar2);
+            TextView TextViewCar3 = FindViewById<TextView>(Resource.Id.textViewCar3);
+            TextView TextViewCar4 = FindViewById<TextView>(Resource.Id.textViewCar4);
+            TextView TextViewCar5 = FindViewById<TextView>(Resource.Id.textViewCar5);
+            TextView TextViewCar6 = FindViewById<TextView>(Resource.Id.textViewCar6);
+            TextView TextViewCar7 = FindViewById<TextView>(Resource.Id.textViewCar7);
+            TextView TextViewCar8 = FindViewById<TextView>(Resource.Id.textViewCar8);
+            TextView TextViewCar9 = FindViewById<TextView>(Resource.Id.textViewCar9);
+            TextView TextViewCar10 = FindViewById<TextView>(Resource.Id.textViewCar10);
+            TextView TextViewCar11 = FindViewById<TextView>(Resource.Id.textViewCar11);
+
+            #endregion
+
+            #region TextViews car range
+
+            TextView TextViewRange1 = FindViewById<TextView>(Resource.Id.textViewRange1);
+            TextView TextViewRange2 = FindViewById<TextView>(Resource.Id.textViewRange2);
+            TextView TextViewRange3 = FindViewById<TextView>(Resource.Id.textViewRange3);
+            TextView TextViewRange4 = FindViewById<TextView>(Resource.Id.textViewRange4);
+            TextView TextViewRange5 = FindViewById<TextView>(Resource.Id.textViewRange5);
+            TextView TextViewRange6 = FindViewById<TextView>(Resource.Id.textViewRange6);
+            TextView TextViewRange7 = FindViewById<TextView>(Resource.Id.textViewRange7);
+            TextView TextViewRange8 = FindViewById<TextView>(Resource.Id.textViewRange8);
+            TextView TextViewRange9 = FindViewById<TextView>(Resource.Id.textViewRange9);
+            TextView TextViewRange10 = FindViewById<TextView>(Resource.Id.textViewRange10);
+            TextView TextViewRange11 = FindViewById<TextView>(Resource.Id.textViewRange11);
+
+            #endregion
+
+            #region Buttons car select
+
+            Button ButtonSelectCar1 = FindViewById<Button>(Resource.Id.buttonSelectCar1);
+            Button ButtonSelectCar2 = FindViewById<Button>(Resource.Id.buttonSelectCar2);
+            Button ButtonSelectCar3 = FindViewById<Button>(Resource.Id.buttonSelectCar3);
+            Button ButtonSelectCar4 = FindViewById<Button>(Resource.Id.buttonSelectCar4);
+            Button ButtonSelectCar5 = FindViewById<Button>(Resource.Id.buttonSelectCar5);
+            Button ButtonSelectCar6 = FindViewById<Button>(Resource.Id.buttonSelectCar6);
+            Button ButtonSelectCar7 = FindViewById<Button>(Resource.Id.buttonSelectCar7);
+            Button ButtonSelectCar8 = FindViewById<Button>(Resource.Id.buttonSelectCar8);
+            Button ButtonSelectCar9 = FindViewById<Button>(Resource.Id.buttonSelectCar9);
+            Button ButtonSelectCar10 = FindViewById<Button>(Resource.Id.buttonSelectCar10);
+            Button ButtonSelectCar11 = FindViewById<Button>(Resource.Id.buttonSelectCar11);
+
+
+            #endregion
+
+            #region MySQL connection
+
+            MySqlConnection connection = new MySqlConnection("Server=db4free.net;Port=3306;Database=czy_dojade;Uid=czy_dojade;Pwd=czy_dojade;");
             connection.Open();
 
-            var query = connection.CreateCommand();
-            query.CommandText = "SELECT marka, model, zasieg FROM cars";
+            #endregion
 
-            var result = query.ExecuteReader();
-
-            result.Read();
-
-            string producer = result.GetString("marka").ToString();
-            string model = result.GetString("model").ToString();
-            string car = producer + " " + model;
-
-
-            string range = result.GetString("zasieg").ToString();
-
-            Car.Text = car;
-
-            Range.Text = "Range: " + range + " km";
-
-            connection.Close();
+            #region Cars
+            
+            Car Car1 = new Car(connection, 1);
+            Car Car2 = new Car(connection, 2);
+            Car Car3 = new Car(connection, 3);
+            Car Car4 = new Car(connection, 4);
+            Car Car5 = new Car(connection, 5);
+            Car Car6 = new Car(connection, 6);
+            Car Car7 = new Car(connection, 7);
+            Car Car8 = new Car(connection, 8);
+            Car Car9 = new Car(connection, 9);
+            Car Car10 = new Car(connection, 10);
+            Car Car11 = new Car(connection, 11);
 
 
-            getBackButton.Click += delegate
+            #endregion
+
+            #region display cars
+
+            TextViewCar1.Text = Car1.GetProducer() + " " + Car1.GetModel();
+            TextViewRange1.Text = "Range: " + Car1.GetRange() + " km";
+
+            TextViewCar2.Text = Car2.GetProducer() + " " + Car2.GetModel();
+            TextViewRange2.Text = "Range: " + Car2.GetRange() + " km";
+
+            TextViewCar3.Text = Car3.GetProducer() + " " + Car3.GetModel();
+            TextViewRange3.Text = "Range: " + Car3.GetRange() + " km";
+
+            TextViewCar4.Text = Car4.GetProducer() + " " + Car4.GetModel();
+            TextViewRange4.Text = "Range: " + Car4.GetRange() + " km";
+
+            TextViewCar5.Text = Car5.GetProducer() + " " + Car5.GetModel();
+            TextViewRange5.Text = "Range: " + Car5.GetRange() + " km";
+
+            TextViewCar6.Text = Car6.GetProducer() + " " + Car6.GetModel();
+            TextViewRange6.Text = "Range: " + Car6.GetRange() + " km";
+
+            TextViewCar7.Text = Car7.GetProducer() + " " + Car7.GetModel();
+            TextViewRange7.Text = "Range: " + Car7.GetRange() + " km";
+
+            TextViewCar8.Text = Car8.GetProducer() + " " + Car8.GetModel();
+            TextViewRange8.Text = "Range: " + Car8.GetRange() + " km";
+
+            TextViewCar9.Text = Car9.GetProducer() + " " + Car9.GetModel();
+            TextViewRange9.Text = "Range: " + Car9.GetRange() + " km";
+
+            TextViewCar10.Text = Car10.GetProducer() + " " + Car10.GetModel();
+            TextViewRange10.Text = "Range: " + Car10.GetRange() + " km";
+
+            TextViewCar11.Text = Car11.GetProducer() + " " + Car11.GetModel();
+            TextViewRange11.Text = "Range: " + Car11.GetRange() + " km";
+
+            #endregion
+
+            #region Buttons
+
+            ButtonSelectCar1.Click += delegate
             {
                 StartActivity(typeof(MainActivity));
             };
 
-            RelativeLayout layout = FindViewById<RelativeLayout>(Resource.Id.relativeLayout1);
+            ButtonSelectCar2.Click += delegate
+            {
+                StartActivity(typeof(MainActivity));
+            };
 
+            ButtonSelectCar3.Click += delegate
+            {
+                StartActivity(typeof(MainActivity));
+            };
+
+            ButtonSelectCar4.Click += delegate
+            {
+                StartActivity(typeof(MainActivity));
+            };
+
+            ButtonSelectCar5.Click += delegate
+            {
+                StartActivity(typeof(MainActivity));
+            };
+
+            ButtonSelectCar6.Click += delegate
+            {
+                StartActivity(typeof(MainActivity));
+            };
+
+            ButtonSelectCar7.Click += delegate
+            {
+                StartActivity(typeof(MainActivity));
+            };
+
+            ButtonSelectCar8.Click += delegate
+            {
+                StartActivity(typeof(MainActivity));
+            };
+
+            ButtonSelectCar9.Click += delegate
+            {
+                StartActivity(typeof(MainActivity));
+            };
+
+            ButtonSelectCar10.Click += delegate
+            {
+                StartActivity(typeof(MainActivity));
+            };
+
+            ButtonSelectCar11.Click += delegate
+            {
+                StartActivity(typeof(MainActivity));
+            };
+
+            #endregion
 
             ImageView image = FindViewById<ImageView>(Resource.Id.imageView1);
 
-
+            connection.Close();
         }
     }
 }
