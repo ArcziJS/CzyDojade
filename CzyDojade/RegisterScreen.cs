77c58e0ac37b8281e2e90f829ee9fddc6b9c5695
QCHICK1337 <35234463+QCHICK1337@users.noreply.github.com>
2023-04-03 21:20:29 +0200

    Added functionality to registration screen.
    Registration screen now inserts data to the user database. Implemented password confirmation check.

--- CzyDojade/RegisterScreen.cs
+++ CzyDojade/RegisterScreen.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Widget;
+using MySqlConnector;
+using static Android.Provider.ContactsContract.CommonDataKinds;
 
 namespace CzyDojade
 {
@@ -13,18 +15,41 @@ namespace CzyDojade
 
             SetContentView(Resource.Layout.register_screen);
 
-            #region Buttons
+            #region MySQL connection
+            MySqlConnection connection = new MySqlConnection("Server=db4free.net;Port=3306;Database=czy_dojade;Uid=czy_dojade;Pwd=czy_dojade;");
+            connection.Open();
+            #endregion
+
+            #region Buttons and EditTexts
             Button ButtonRegister = FindViewById<Button>(Resource.Id.ButtonRegister);
             Button ButtonRegister2 = FindViewById<Button>(Resource.Id.ButtonRegister2);
+            EditText Username = FindViewById<EditText>(Resource.Id.Username);
+            EditText Email = FindViewById<EditText>(Resource.Id.Email);
+            EditText Password = FindViewById<EditText>(Resource.Id.Password);
+            EditText ConfirmPassword = FindViewById<EditText>(Resource.Id.ConfirmPassword);
             #endregion
 
             #region Functionality of Buttons
             ButtonRegister2.Click += delegate
             {
-                StartActivity(typeof(LoginScreen)); 
+                StartActivity(typeof(LoginScreen));
             };
+
             ButtonRegister.Click += delegate
             {
+                if (Password.Text != ConfirmPassword.Text) // Check if passwords match
+                {
+                    Toast.MakeText(this, "Passwords don't match", ToastLength.Short).Show();
+                    return;
+                }
+
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "INSERT INTO uzytkownik (Login, Email, Haslo) VALUES (@username, @email, @password)";
+                command.Parameters.AddWithValue("@username", Username.Text);
+                command.Parameters.AddWithValue("@last_name", "");
+                command.Parameters.AddWithValue("@email", Email.Text);
+                command.Parameters.AddWithValue("@password", Password.Text);
+                command.ExecuteNonQuery();
                 StartActivity(typeof(MapScreen));
             };
             #endregion
@@ -32,3 +57,7 @@ namespace CzyDojade
         }
     }
 }
+
+
+
+
