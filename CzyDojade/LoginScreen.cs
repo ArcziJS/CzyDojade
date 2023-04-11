bd3cac6ed97ce67957c4c38816da90e852e467d6
Dawid Skowron <dawid.skowron@icloud.com>
2023-04-11 20:36:10 +0200

    Done login funcionality 100%

--- CzyDojade/LoginScreen.cs
+++ CzyDojade/LoginScreen.cs
@@ -1,8 +1,9 @@
 ﻿using Android.App;
+using Android.Content.Res;
 using Android.OS;
 using Android.Widget;
 using MySqlConnector;
-
+using System;
 
 namespace CzyDojade
 {
@@ -26,25 +27,58 @@ namespace CzyDojade
             Button ButtonLogin2 = FindViewById<Button>(Resource.Id.ButtonLogin2);
             Button ButtonLogin3 = FindViewById<Button>(Resource.Id.ButtonLogin3);
             Button ButtonLogin6 = FindViewById<Button>(Resource.Id.ButtonLogin6);
+            EditText Email = FindViewById<EditText>(Resource.Id.Email);
+            EditText Password = FindViewById<EditText>(Resource.Id.Password);
+
             #endregion
 
             #region Functionality of Buttons
+            //ButtonLogin.Click += delegate
+            //{
+            //    StartActivity(typeof(MapScreen));  
+            //};
+
             ButtonLogin.Click += delegate
             {
-                StartActivity(typeof(MapScreen));  
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT COUNT(*) FROM uzytkownik WHERE Email = @email AND Haslo = @password";
+                command.Parameters.AddWithValue("@email", Email.Text);
+                command.Parameters.AddWithValue("@password", Password.Text);
+                int count = Convert.ToInt32(command.ExecuteScalar());
+
+                if (count == 1)
+                {
+                    StartActivity(typeof(MapScreen));
+                }
+                else
+                {
+                    Toast.MakeText(this, "Wrong email or password. Please try again.", ToastLength.Short).Show();
+                }
+
+                //MySqlCommand command = connection.CreateCommand();
+                //command.CommandText = "SELECT FROM uzytkownik (Email, Haslo) VALUES (@textEmailAddress, @login_Password)";
+                //command.Parameters.AddWithValue("@textEmailAddress", textEmailAddress.Text);
+                //command.Parameters.AddWithValue("@login_Password", login_Password.Text);
+
+                //int rows = Convert.ToInt32(command.ExecuteScalar());
+
+                //if (rows == 1)
+                //{
+                //    StartActivity(typeof(MapScreen));
+                //}
+                //else
+                //{
+                //    Toast.MakeText(this, "Try again wrong email or password!", ToastLength.Short).Show();
+                //}
+
+
             };
-            ButtonLogin2.Click += delegate
-            {
-                StartActivity(typeof(RegisterScreen));  
-            };
-            ButtonLogin6.Click += delegate
-            {
-                StartActivity(typeof(CarSelector));
-            };
-            //ButtonLogin3.Click += delegate
+            
+            //ButtonLogin6.Click += delegate
             //{
-            //    StartActivity(typeof(MainActivity));
+            //    StartActivity(typeof(CarSelector));
             //};
+         
             #endregion
 
         }
