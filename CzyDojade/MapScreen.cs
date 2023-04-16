ecac44229491a13815be9ca2adbcfd034508a32c
Artur Siudak <artur.siudak@student.po.edu.pl>
2023-04-16 23:24:52 +0200

    Adding location search option.

--- CzyDojade/MapScreen.cs
+++ CzyDojade/MapScreen.cs
@@ -1,10 +1,13 @@
 ﻿using Android.App;
 using Android.OS;
-using Android.Views;
+using Android.Widget;
 using Com.Mapbox.Mapboxsdk;
 using Com.Mapbox.Mapboxsdk.Camera;
 using Com.Mapbox.Mapboxsdk.Geometry;
 using Com.Mapbox.Mapboxsdk.Maps;
+using System.Linq;
+//using Mapbox.Services.Geocoding;
+using Xamarin.Essentials;
 
 namespace CzyDojade
 {
@@ -17,6 +20,7 @@ namespace CzyDojade
             base.OnCreate(savedInstanceState);
 
             Mapbox.GetInstance(this, "pk.eyJ1IjoiY3p5ZG9qYWRlIiwiYSI6ImNsZ2RyZmk2azBiaDUzZXEzY3ZqdXhlOWYifQ.DXoRCQQlVsnu4Ujv3-r7OQ");
+
             SetContentView(Resource.Layout.map_screen);
 
             mapView = FindViewById<MapView>(Resource.Id.mapView);
@@ -35,7 +39,25 @@ namespace CzyDojade
                 .Build();
 
             mapboxMap.AnimateCamera(CameraUpdateFactory.NewCameraPosition(cameraPosition), 5000);
+
+            #region Search
+
+            SearchView searchView = FindViewById<SearchView>(Resource.Id.searchView);
+            searchView.QueryTextSubmit += async (sender, args) =>
+            {
+                var results = await Geocoding.GetLocationsAsync(args.Query);
+
+                var location = results.FirstOrDefault();
+                var position = new LatLng(location.Latitude, location.Longitude);
+                mapboxMap.AnimateCamera(CameraUpdateFactory.NewLatLngZoom(position, 15),5000);
+            };
+
+            #endregion
         }
+
+
+
+
 
     }
 }
