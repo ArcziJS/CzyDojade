5b7cd850bff5a2731dba0d6fc09164273d0a90d6
Artur Siudak <artur.siudak@student.po.edu.pl>
2023-04-16 20:33:46 +0200

    Adding a Mapbox mapview.

--- CzyDojade/MapScreen.cs
+++ CzyDojade/MapScreen.cs
@@ -1,16 +1,41 @@
 ﻿using Android.App;
 using Android.OS;
+using Android.Views;
+using Com.Mapbox.Mapboxsdk;
+using Com.Mapbox.Mapboxsdk.Camera;
+using Com.Mapbox.Mapboxsdk.Geometry;
+using Com.Mapbox.Mapboxsdk.Maps;
 
 namespace CzyDojade
 {
     [Activity(Label = "Activity1")]
-    public class MapScreen : Activity
+    public class MapScreen : Activity, IOnMapReadyCallback
     {
+        MapView mapView;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
+            Mapbox.GetInstance(this, "pk.eyJ1IjoiY3p5ZG9qYWRlIiwiYSI6ImNsZ2RyZmk2azBiaDUzZXEzY3ZqdXhlOWYifQ.DXoRCQQlVsnu4Ujv3-r7OQ");
             SetContentView(Resource.Layout.map_screen);
+
+            mapView = FindViewById<MapView>(Resource.Id.mapView);
+            mapView.OnCreate(savedInstanceState);
+            mapView.GetMapAsync(this);
         }
+
+        public void OnMapReady(MapboxMap mapboxMap)
+        {
+            mapboxMap.SetStyle(new Style.Builder().FromUrl("mapbox://styles/mapbox/dark-v10"));
+
+
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                .Target(new LatLng(53.123326, 23.08638))
+                .Zoom(18)
+                .Build();
+
+            mapboxMap.AnimateCamera(CameraUpdateFactory.NewCameraPosition(cameraPosition), 5000);
+        }
+
     }
 }
