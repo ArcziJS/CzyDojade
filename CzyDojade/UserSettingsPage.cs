4a050cb3cb76702b0d4cb76192a43a61a5c21ac4
QCHICK1337 <35234463+QCHICK1337@users.noreply.github.com>
2023-04-16 12:14:57 +0200

    Added user settings page

---
+++ CzyDojade/UserSettingsPage.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.OS;
+using MySqlConnector;
+
+namespace CzyDojade
+{
+    [Activity(Label = "UserSettingsPage")]
+    public class UserSettingsPage : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.user_settings_page);
+
+            #region MySQL connection
+            MySqlConnection connection = new MySqlConnection("Server=db4free.net;Port=3306;Database=czy_dojade;Uid=czy_dojade;Pwd=czy_dojade;");
+            connection.Open();
+            #endregion
+
+            #region Buttons etc.
+
+            #endregion
+
+            #region Functionality of Buttons
+
+            #endregion
+
+        }
+    }
+}
+
+
+
+
